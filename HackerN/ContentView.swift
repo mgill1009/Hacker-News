4c64ab2d908f42e4f991193d4344dc95124d54ff
mgill1009 <caviar1009@gmail.com>
2024-05-14 15:26:55 -0700

    Initial Commit

---
+++ HackerN/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackerN
+//
+//  Created by Manpreet Gill on 2024-05-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackerN/HackerNApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNApp.swift
+//  HackerN
+//
+//  Created by Manpreet Gill on 2024-05-14.
+//
+
+import SwiftUI
+
+@main
+struct HackerNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
